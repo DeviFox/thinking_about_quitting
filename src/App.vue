2c425753086e3a8d451c2ad06633fdba27b94e6c
svyatnenko.ov <svyatnenko.ov@dns-shop.ru>
2025-06-06 20:41:57 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="layout">
+    <h1 class="title">Без мыслей об увольнении</h1>
+
+  </div>
+</template>
+
+<style scoped>
+.layout {
+padding-top: 150px;
+}
+.title {
+  text-align: center;
+
+
+}
+</style>
